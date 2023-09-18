a0ad4dc9e6690dd2bbd9e3117b30908db110d3eb
Austin Biketi Naulikha <nbiketia@gmail.com>
2023-09-18 06:43:13 +0300

    mak changes to frame

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -9,29 +9,17 @@ const Frame = () => {
           <div className="rectangle" />
           <div className="div" />
           <div className="circle-edit" />
-          <div className="navbar">
-            <div className="text-wrapper">HOME</div>
-            <div className="text-wrapper">ABOUT</div>
-            <div className="text-wrapper">PHOTOS</div>
-            <div className="text-wrapper">CONTACT</div>
-          </div>
           <div className="rectangle-2" />
         </div>
-        <div className="logo">
-          <div className="overlap-group-2">
-            <div className="text-wrapper-2">K</div>
-            <div className="text-wrapper-3">ama</div>
-            <div className="text-wrapper-4">vindi</div>
-          </div>
-        </div>
         <div className="info">
-          <p className="welcome-to-the">Welcome to the family run&nbsp;&nbsp;coffee farm</p>
+          <p className="welcome-to-the">Welcome to the family-run coffee farm</p>
           <div className="text-wrapper-5">Established in 1956</div>
+          <div className="text-wrapper-6">10,000</div>
+          <div className="text-wrapper-7">Coffee Trees</div>
         </div>
-        <div className="text-wrapper-6">10,000</div>
-        <div className="text-wrapper-7">Coffee Trees</div>
       </div>
     </div>
   );
 };
+
 export default Frame;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,13 @@ import '../styles/NavBar.css';  // Import the CSS file
 const NavBar = () => {
   return (
     <nav className="nav"> {/* Use className instead of style */}
-      <div className="logo-container">
-        <h1 className='Logo'>Kama</h1>
-        <h1 className='Logo2'>vindi</h1>
-      </div>
+        <div className="logo">
+          <div className="overlap-group-2">
+            <div className="text-wrapper-2">K</div>
+            <div className="text-wrapper-3">ama</div>
+            <div className="text-wrapper-4">vindi</div>
+          </div>
+        </div>
       <ul className="nav-list"> {/* Use className instead of style */}
         <li className="nav-item"> {/* Use className instead of style */}
           <Link to="/">Home</Link>
